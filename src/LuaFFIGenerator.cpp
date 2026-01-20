02720015c86ef029396126a1ac2f44437fa18c70
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2026-01-20 10:55:34 +0300

    add prefix

--- src/LuaFFIGenerator.cpp
+++ src/LuaFFIGenerator.cpp
@@ -1411,7 +1411,7 @@ int main(int argc, char *argv[]) {
 			lua_cdef_wrapper += annotation;
 			lua_cdef_wrapper += "---@return " + lua_id(ob.name + "_id") +  "\n";
 			lua_cdef_wrapper += "function " + lua_namespace + ".try_create(" + lua_args + ")\n";
-			lua_cdef_wrapper += "\treturn ffi.C.try_create_" + ob.name + "(" + lua_args + ")\n";
+			lua_cdef_wrapper += "\treturn ffi.C." + project_prefix + "try_create_" + ob.name + "(" + lua_args + ")\n";
 			lua_cdef_wrapper += "end\n";
 
 			lua_cdef += "int32_t " + project_prefix + "force_create_" + ob.name + "(" + pargs + "); \n";
@@ -1419,7 +1419,7 @@ int main(int argc, char *argv[]) {
 			lua_cdef_wrapper += annotation;
 			lua_cdef_wrapper += "---@return " + lua_id(ob.name + "_id") +  "\n";
 			lua_cdef_wrapper += "function " + lua_namespace + ".force_create(" + lua_args + ")\n";
-			lua_cdef_wrapper += "\treturn ffi.C.force_create_" + ob.name + "(" + lua_args + ")\n";
+			lua_cdef_wrapper += "\treturn ffi.C." + project_prefix + "force_create_" + ob.name + "(" + lua_args + ")\n";
 			lua_cdef_wrapper += "end\n";
 		};
 
